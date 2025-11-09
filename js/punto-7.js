973c65be8b339d4933a7431f75d4d2ef075768f1
Ronald <guerrerotoloza.ro@gmail.com>
2025-11-08 22:16:58 -0500

    Punto #7

---
+++ js/punto-7.js
@@ -0,0 +1,10 @@
+var numero1 = prompt("ingresa un numero")
+var numero2 = prompt("ingresa un numero")
+
+numero1 = +numero1
+numero2 = +numero2
+
+for (let i = numero1; i <= numero2; i++){
+    document.write(i);
+    console.log(i);    
+}
