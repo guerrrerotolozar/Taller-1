a589d0f739e845ef56a881611b39d28c4814c2e6
Ronald <guerrerotoloza.ro@gmail.com>
2025-11-08 19:57:21 -0500

    Punto #1

---
+++ 1-input.js
@@ -0,0 +1,16 @@
+var numero1 = prompt(`numero 1 =>`);
+var numero2 = prompt(`numero 2 =>`);
+// pide los dos numeros PERO los lanza en string
+
+
+// puedo poner a hacer uno por uno de los cambios de string a numero (entero en lo posible)
+// o
+// =>
+
+let suma = +numero1 + +numero2  //para pasar el string a numero solo se le pone un [+] antes de la variable que necesito
+
+
+
+console.log(suma) // vi uno de ponerlo en pantalla tambien y se ve bacano para evitar entrar a la consola en navegador
+
+document.write(suma)  // pero no se porque me tacha ese WRITE // aun asi sirve xd
