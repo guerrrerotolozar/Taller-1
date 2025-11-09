df51d975689e03bd4f89b3f5fd60d214760dafbc
Ronald <guerrerotoloza.ro@gmail.com>
2025-11-08 20:50:51 -0500

    Punto #3

---
+++ js/punto-3.js
@@ -0,0 +1,9 @@
+var numero = prompt("Ingresa un numero que creas que es multiplo de 5 =>")
+var numero = +numero  //no se porquje pero esta vez toco re<signarle el valor a la variable asi porque estaba mandando el numero + un undefinend
+
+if (numero % 5 == 0){
+    document.write("si es")
+}
+else{
+    document.write("no es")
+}
