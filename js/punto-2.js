3dd905d1a9194e299a39f314657b2741652a6925
Ronald <guerrerotoloza.ro@gmail.com>
2025-11-08 20:18:55 -0500

    Punto #2 [De paso cree la carpeta de js]

---
+++ js/punto-2.js
@@ -0,0 +1,9 @@
+var año = prompt("Ingresa tu año de nacimiento =>");
+var nombre = prompt("Ingresa tu nombre =>")
++año;
+const añofijo = 2020;
+resta = añofijo - año
+
+console.log(` Hola ${nombre}, Teniendo en cuenta que estamos en ${añofijo}, si nanciste en el ${año} tu edad seria ${resta}`)
+
+document.write(`Hola ${nombre}, Teniendo en cuenta que estamos en ${añofijo}, si nanciste en el ${año} tu edad seria ${resta}`)
